1e3175677fbd6a80ff86d0d32ac94671f0ed3947
elvis.barudzija <elvis@barudzija.se>
2022-12-17 01:34:32 +0100

    Day1: Add input file plus some clean up

--- aoc2022day1.py
+++ aoc2022day1.py
@@ -1,23 +1,19 @@
 f = open("input-1.txt","r")
 lines = f.readlines()
 
-biggestelf=0
-currelf=0
+biggestelf = currelf = 0
 
 for line in lines:
     splitted_line = line.split('\n')
     try:
         currelf=currelf+int(splitted_line[0])
     except ValueError:
-        flag = False
-
+        None
     if line in ['\n', '\r\n']:
         if currelf > biggestelf:
             # We have new leader!
             biggestelf=currelf
         print(currelf)
         currelf = 0
-        # We've found a blank line, stop counting!
-        # save the counted elf into a list!
 
 print("## Biggest elf is:", biggestelf)
